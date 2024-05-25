b651cdeea0351c9971f6f931cfa86d13a6cf0026
Prithviraj Saha <prithvirajsaha1000@gmail.com>
2024-05-25 21:53:43 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,29 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import boardsSlice from "./boardsSlice";
-import themeSlice from "./themeSlice";
-
-const store = configureStore({
-  reducer: {
-    boards: boardsSlice.reducer,
-    theme: themeSlice.reducer
-  }
-})
-// src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import boardsReducer from './boardsSlice';
 import userReducer from './userSlice';
-import themeReducer from './themeSlice'; // If you have a themeSlice
+import themeReducer from './themeSlice';
 
 const store = configureStore({
   reducer: {
     boards: boardsReducer,
     user: userReducer,
-    theme: themeReducer, // If you have a themeSlice
+    theme: themeReducer,
   },
 });
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default store
